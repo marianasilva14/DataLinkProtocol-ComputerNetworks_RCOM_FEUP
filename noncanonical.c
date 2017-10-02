e3729d47e9a195930985043df6a74b91b6744ab4
Francisca Cerquinho <franciscacerquinho@gmail.com>
2017-10-02 11:41:46 +0100

    Add files via upload

---
+++ noncanonical.c
@@ -0,0 +1,106 @@
+	/*Non-Canonical Input Processing*/
+
+	#include <sys/types.h>
+	#include <sys/stat.h>
+	#include <fcntl.h>
+	#include <termios.h>
+	#include <stdio.h>
+
+	#define BAUDRATE B38400
+	#define _POSIX_SOURCE 1 /* POSIX compliant source */
+	#define FALSE 0
+	#define TRUE 1
+	#define FLAG 0x7e
+	#define A 0x03
+	#define C_SET 0x03
+
+	volatile int STOP=FALSE;
+
+int main(int argc, char** argv)
+{
+	    int fd,c, res;
+	    struct termios oldtio,newtio;
+	    char buf[255];
+	buf[254] = '\0';
+	unsigned char c;
+		
+
+	    if ( (argc < 2) || 
+	  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+	  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+	      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+	      exit(1);
+	    }
+
+
+	  /*
+	    Open serial port device for reading and writing and not as controlling tty
+	    because we don't want to get killed if linenoise sends CTRL-C.
+	  */
+	  
+	    
+	    fd = open(argv[1], O_RDWR | O_NOCTTY );
+	    if (fd <0) {perror(argv[1]); exit(-1); }
+
+	    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+	      perror("tcgetattr");
+	      exit(-1);
+	    }
+
+	    bzero(&newtio, sizeof(newtio));
+	    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	    newtio.c_iflag = IGNPAR;
+	    newtio.c_oflag = 0;
+
+	    /* set input mode (non-canonical, no echo,...) */
+	    newtio.c_lflag = 0;
+
+	    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+	  /* 
+	    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+	    leitura do(s) próximo(s) caracter(es)
+	  */
+
+
+
+	    tcflush(fd, TCIOFLUSH);
+
+	    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+	      perror("tcsetattr");
+	      exit(-1);
+	    }
+
+	    printf("New termios structure set\n");
+
+	
+		char retString[255];
+		int n=0;
+
+
+		while (STOP==FALSE) {       // loop for input 
+
+			res = read(fd,buf,1);
+			buf[res]=0;   // so we can printf... 
+			strcat(retString, buf);  
+		      	printf(":%s:%d\n", buf, res);
+			if (buf[0] == '\0') STOP=TRUE;
+		}	
+
+
+		printf("%s\n", retString);
+		int len = strlen(retString);
+		write(fd, retString, len+1);
+
+	
+
+		sleep(3);
+
+
+	    tcsetattr(fd,TCSANOW,&oldtio);
+	    close(fd);
+	    return 0;
+	}
