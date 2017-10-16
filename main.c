a49b728f2dc2b71c57a904e0acdd659fc0bc919b
nunocr <nunocr963@gmail.com>
2017-10-16 15:14:04 +0100

    Update

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <signal.h>
+#include <stdio.h>
+
+int main(int argc, char** argv){
+
+  if ( (argc < 2) ||
+       ((strcmp("/dev/ttyS0", argv[1])!=0) &&
+        (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+    printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+    exit(1);
+  }
+
+}
