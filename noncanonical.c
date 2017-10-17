4551cf00309811deab4571c8c264384cb1847393
FranciscaCerquinho <franciscacerquinho@gmail.com>
2017-10-17 16:05:32 +0100

    llopen done

--- noncanonical.c
+++ noncanonical.c
@@ -1,147 +1,149 @@
-	/*Non-Canonical Input Processing*/
+/*Non-Canonical Input Processing*/
 
-	#include <sys/types.h>
-	#include <sys/stat.h>
-	#include <fcntl.h>
-	#include <termios.h>
-	#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
 
-	#define BAUDRATE B38400
-	#define _POSIX_SOURCE 1 /* POSIX compliant source */
-	#define FALSE 0
-	#define TRUE 1
-	#define FLAG 0x7e
-	#define A 0x03
-	#define C_SET 0x03
+#define BAUDRATE B38400
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+#define FLAG 0x7e
+#define A 0x03
+#define C_SET 0x03
 
-	volatile int STOP=FALSE;
+volatile int STOP=FALSE;
+struct termios oldtio,newtio;
 
-int main(int argc, char** argv)
+void stateMachineReceiver(int fd,unsigned char UA[5])
 {
-	    int fd,c, res;
-	    struct termios oldtio,newtio;
 	unsigned char foo;
-	    char buf[255];
-	buf[254] = '\0';
+	int state = 0;
+	while(state!=5){
 
+		read(fd, &foo, 1);
+		switch(state){
+			case 0:
+			if(foo==FLAG) state = 1;
+			break;
+			case 1:
+			if(foo==FLAG) state = 1;
+			if(foo==A) state = 2;
+			else state = 0;
+			break;
+			case 2:
+			if(foo==FLAG) state = 1;
+			if(foo==C_SET) state = 3;
+			else state = 0;
+			break;
+			case 3:
+			if(foo==FLAG) state = 1;
+			if(!A^C_SET) state = 4;
+			else state = 0;
+			break;
+			case 4:
+			if(foo==FLAG) state = 5;
+			else state = 0;
+			break;
+		}
 
+	}
+
+		printf("SET received successfully!\n");
+		write(fd, UA, 5);
+}
+int llopen(int fd){
 	unsigned char UA[5];
 	UA[0] = FLAG;
 	UA[1] = A;
 	UA[2] = C_SET;
 	UA[3] = UA[1]^UA[2];
 	UA[4] = FLAG;
-		
 
-	    if ( (argc < 2) || 
-	  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
-	  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-	      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-	      exit(1);
-	    }
+	stateMachineReceiver(fd,UA);
+}
+int main(int argc, char** argv)
+{
+	int fd,c, res;
+	char buf[255];
+	buf[254] = '\0';
 
-
-	  /*
-	    Open serial port device for reading and writing and not as controlling tty
-	    because we don't want to get killed if linenoise sends CTRL-C.
-	  */
-	  
-	    
-	    fd = open(argv[1], O_RDWR | O_NOCTTY );
-	    if (fd <0) {perror(argv[1]); exit(-1); }
-
-	    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-	      perror("tcgetattr");
-	      exit(-1);
-	    }
-
-	    bzero(&newtio, sizeof(newtio));
-	    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-	    newtio.c_iflag = IGNPAR;
-	    newtio.c_oflag = 0;
-
-	    /* set input mode (non-canonical, no echo,...) */
-	    newtio.c_lflag = 0;
-
-	    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-	    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
-
-
-
-	  /* 
-	    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
-	    leitura do(s) próximo(s) caracter(es)
-	  */
-
-
-
-	    tcflush(fd, TCIOFLUSH);
-
-	    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-	      perror("tcsetattr");
-	      exit(-1);
-	    }
-
-	    printf("New termios structure set\n");
-
-		int state = 0;
-
-		while(state!=5){
-
-			read(fd, &foo, 1);
-			switch(state){
-				case 0:
-					if(foo==FLAG) state = 1;
-					break;
-				case 1:
-					if(foo==FLAG) state = 1;
-					if(foo==A) state = 2;
-					else state = 0;
-					break;
-				case 2:
-					if(foo==FLAG) state = 1;
-					if(foo==C_SET) state = 3;
-					else state = 0;
-					break;
-				case 3:
-					if(foo==FLAG) state = 1;					
-					if(!A^C_SET) state = 4;
-					else state = 0;
-					break;
-				case 4:
-					if(foo==FLAG) state = 5;
-					else state = 0;
-					break;
-			}
-
-		}
-
-		printf("SET received successfully!\n");
-		
-		write(fd, UA, 5);
-
-		char retString[255];
-		int n=0;
-
-
-		while (STOP==FALSE) {       // loop for input 
-
-			res = read(fd,buf,1);
-			buf[res]=0;   // so we can printf... 
-			strcat(retString, buf);  
-		      	printf(":%s:%d\n", buf, res);
-			if (buf[0] == '\0') STOP=TRUE;
-		}	
-
-
-		printf("%s\n", retString);
-		int len = strlen(retString);
-		write(fd, retString, len+1);
-
-		sleep(3);
-
-
-	    tcsetattr(fd,TCSANOW,&oldtio);
-	    close(fd);
-	    return 0;
+	if ( (argc < 2) ||
+	((strcmp("/dev/ttyS0", argv[1])!=0) &&
+	(strcmp("/dev/ttyS1", argv[1])!=0) )) {
+		printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+		exit(1);
 	}
+
+
+	/*
+	Open serial port device for reading and writing and not as controlling tty
+	because we don't want to get killed if linenoise sends CTRL-C.
+	*/
+
+
+	fd = open(argv[1], O_RDWR | O_NOCTTY );
+	if (fd <0) {perror(argv[1]); exit(-1); }
+
+	if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+		perror("tcgetattr");
+		exit(-1);
+	}
+
+	bzero(&newtio, sizeof(newtio));
+	newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+	newtio.c_iflag = IGNPAR;
+	newtio.c_oflag = 0;
+
+	/* set input mode (non-canonical, no echo,...) */
+	newtio.c_lflag = 0;
+
+	newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+	newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+	/*
+	VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+	leitura do(s) prï¿½ximo(s) caracter(es)
+	*/
+
+
+
+	tcflush(fd, TCIOFLUSH);
+
+	if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+		perror("tcsetattr");
+		exit(-1);
+	}
+
+	printf("New termios structure set\n");
+	llopen(fd);
+
+	char retString[255];
+	int n=0;
+
+
+	while (STOP==FALSE) {       // loop for input
+
+		res = read(fd,buf,1);
+		buf[res]=0;   // so we can printf...
+		strcat(retString, buf);
+		printf(":%s:%d\n", buf, res);
+		if (buf[0] == '\0') STOP=TRUE;
+	}
+
+
+	printf("%s\n", retString);
+	int len = strlen(retString);
+	write(fd, retString, len+1);
+
+	sleep(3);
+
+
+	tcsetattr(fd,TCSANOW,&oldtio);
+	close(fd);
+	return 0;
+}
--- writenoncanonical.c
+++ writenoncanonical.c
@@ -7,19 +7,18 @@
 #include <signal.h>
 #include <stdio.h>
 
-
 #define BAUDRATE B38400
 #define MODEMDEVICE "/dev/ttyS1"
 #define _POSIX_SOURCE 1 /* POSIX compliant source */
 #define FLAG 0x7e
 #define A 0x03
 #define C_SET 0x03
-
 #define FALSE 0
 #define TRUE 1
 
 volatile int STOP=FALSE;
 int flag_alarm=0, conta_alarm=0, flag_continue = 0;
+struct termios oldtio,newtio;
 
 void atende()                   // atende alarme
 {
@@ -30,151 +29,155 @@ void atende()                   // atende alarme
 		printf("Exiting...");
 		exit(-1);}
 }
-int main(int argc, char** argv)
-{
 
-    int fd,c, res;
+void stateMachineTransmissor(int fd,unsigned char SET[5]){
+	(void)signal(SIGALRM, atende);  // instala  rotina que atende interrupcao
+	int state=0;
 	unsigned char foo;
-    struct termios oldtio,newtio;
-    char buf[255];
-	unsigned char SET[5];
 	int bytes;
 
+	while(!flag_continue){
+
+		bytes = write(fd,SET,5);
+		alarm(3);
+		flag_alarm = 0;
+		while(state!=5 && !flag_alarm){
+
+			read(fd, &foo,1);
+
+			switch(state){
+
+				case 0: if(foo==FLAG)
+				state=1;
+				break;
+				case 1: if(foo==FLAG)
+				state=1;
+				if(foo==A)
+				state=2;
+				else
+				state=0;
+				break;
+				case 2:	if(foo==FLAG) state=1;
+				if(foo==C_SET) state=3;
+				else state=0;
+				break;
+				case 3: if(foo==FLAG) state=1;
+				if(!A^C_SET) state=4;
+				else state=0;
+				break;
+				case 4: if(foo==FLAG) {
+					state=5;
+					flag_continue = 1;
+					alarm(0);
+				}
+				else state=0;
+				break;
+				default: continue;
+			}
+		}
+	}
+}
+
+int llopen(int fd)
+{
+	unsigned char SET[5];
 	SET[0]=FLAG;
 	SET[1]=A;
 	SET[2]=C_SET;
 	SET[3]=SET[1]^SET[2];
 	SET[4]=FLAG;
 
+	stateMachineTransmissor(fd,SET);
 
-    int i, sum = 0, speed = 0;
-
-    if ( (argc < 2) ||
-         ((strcmp("/dev/ttyS0", argv[1])!=0) &&
-          (strcmp("/dev/ttyS1", argv[1])!=0) )) {
-      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
-      exit(1);
-    }
-
-	(void)signal(SIGALRM, atende);  // instala  rotina que atende interrupcao
-
-
-  /*
-    Open serial port device for reading and writing and not as controlling tty
-    because we don't want to get killed if linenoise sends CTRL-C.
-  */
-
-
-    fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
-    if (fd <0) {perror(argv[1]); exit(-1); }
-
-    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
-      perror("tcgetattr");
-      exit(-1);
-    }
-
-    bzero(&newtio, sizeof(newtio));
-    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
-    newtio.c_iflag = IGNPAR;
-    newtio.c_oflag = 0;
-
-    /* set input mode (non-canonical, no echo,...) */
-    newtio.c_lflag = 0;
-
-    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
-    newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
-
-  /*
-    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
-    leitura do(s) próximo(s) caracter(es)
-  */
-
-    tcflush(fd, TCIOFLUSH);
-
-    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
-
-    printf("New termios structure set\n");
-
-
-
-
-	int state=0;
-
-
-
-
-		while(!flag_continue){
-
-			bytes = write(fd,SET,5);
-			alarm(3);
-			flag_alarm = 0;
-			while(state!=5 && !flag_alarm){
-
-				read(fd, &foo,1);
-
-					switch(state){
-
-						case 0: if(foo==FLAG)
-								state=1;
-								break;
-						case 1: if(foo==FLAG)
-								state=1;
-								if(foo==A)
-								state=2;
-								else
-								state=0;
-								break;
-						case 2:	if(foo==FLAG) state=1;
-								if(foo==C_SET) state=3;
-								else state=0;
-								break;
-						case 3: if(foo==FLAG) state=1;
-								if(!A^C_SET) state=4;
-								else state=0;
-								break;
-						case 4: if(foo==FLAG) {
-								state=5;
-								flag_continue = 1;
-								alarm(0);
-								}
-								else state=0;
-								break;
-						default: continue;
-					}
-				}
+	if(flag_continue==1)
+	return 0;
+	else
+	return -1;
 }
 
+	int main(int argc, char** argv)
+	{
+		int fd,c, res;
+		char buf[255];
 
-	gets(buf);
-    int length;
-    length = strlen(buf);
-	buf[length] = 0;
+		int i, sum = 0, speed = 0;
+
+		if ( (argc < 2) ||
+		((strcmp("/dev/ttyS0", argv[1])!=0) &&
+		(strcmp("/dev/ttyS1", argv[1])!=0) )) {
+			printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+			exit(1);
+		}
+
+		/*
+		Open serial port device for reading and writing and not as controlling tty
+		because we don't want to get killed if linenoise sends CTRL-C.
+		*/
 
 
-    res = write(fd,buf,length+1);
-    printf("%d bytes written\n", res);
+		fd = open(argv[1], O_RDWR | O_NOCTTY | O_NONBLOCK);
+		if (fd <0) {perror(argv[1]); exit(-1); }
 
-    while(STOP==FALSE){
-		sleep(0.5);
-        res = read(fd,buf,1);
+		if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+			perror("tcgetattr");
+			exit(-1);
+		}
+
+		bzero(&newtio, sizeof(newtio));
+		newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+		newtio.c_iflag = IGNPAR;
+		newtio.c_oflag = 0;
+
+		/* set input mode (non-canonical, no echo,...) */
+		newtio.c_lflag = 0;
+
+		newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+		newtio.c_cc[VMIN]     = 5;   /* blocking read until 5 chars received */
 
 
-        printf("Resultado: %d\n", res);
-        buf[res]=0;
-        printf(":%s:%d\n", buf, res);
-        if(buf[0]=='\0')
-            STOP=TRUE;
+		/*
+		VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a
+		leitura do(s) próximo(s) caracter(es)
+		*/
 
-    }
+		tcflush(fd, TCIOFLUSH);
 
-    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
-      perror("tcsetattr");
-      exit(-1);
-    }
+		if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+			perror("tcsetattr");
+			exit(-1);
+		}
 
-    close(fd);
-    return 0;
-}
+		printf("New termios structure set\n");
+		llopen(fd);
+
+
+		gets(buf);
+		int length;
+		length = strlen(buf);
+		buf[length] = 0;
+
+
+		res = write(fd,buf,length+1);
+		printf("%d bytes written\n", res);
+
+		while(STOP==FALSE){
+			sleep(0.5);
+			res = read(fd,buf,1);
+
+
+			printf("Resultado: %d\n", res);
+			buf[res]=0;
+			printf(":%s:%d\n", buf, res);
+			if(buf[0]=='\0')
+			STOP=TRUE;
+
+		}
+
+		if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+			perror("tcsetattr");
+			exit(-1);
+		}
+
+		close(fd);
+		return 0;
+	}
