5e06075ab0b8891e220cc626def574adddca8f40
Nuno Marques <ee11291@fe.up.pt>
2017-12-04 17:24:19 +0000

    Download finished

--- Project2/ftp.c
+++ Project2/ftp.c
@@ -77,42 +77,233 @@ int parseURL(char *path, url_info *info){
 }
 
 
-int connect(const char *ip, int port){
+int getIpByHost(url_info* url) 
+{
+  struct hostent *h;
+
+
+  printf("Host name url  : %s\n", url->host);
+
+  if ((h=gethostbyname(url->host)) == NULL) {  
+    herror("gethostbyname");
+    exit(1);
+  }
+
+    
+  char* ip = inet_ntoa(*((struct in_addr *)h->h_addr));
+  url->ip=malloc(sizeof(ip));
+  strcpy(url->ip, ip);
+  printf("%s\n",url->ip);
+  return 0;
+}
+
+
+
+int connectFTP(const char* ip, int port){
+
+  int sockfd;
+  struct  sockaddr_in server_addr;
+
+    /*server address handling*/
+  bzero((char*)&server_addr,sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR); /*32 bit Internet address network byte ordered*/
+    server_addr.sin_port = htons(SERVER_PORT);    /*server TCP port must be network byte ordered */
+
+    /*open an TCP socket*/
+  if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
+    perror("socket()");
+    exit(0);
+  }
+
+    /*connect to the server*/
+  if(connect(sockfd, 
+   (struct sockaddr *)&server_addr, 
+   sizeof(server_addr)) < 0){
+    perror("connect()");
+  exit(0);
+}
+
+  return sockfd;
+}
+
+int readFtpReply(int control, char* str, size_t size){
+
+  FILE* fp = fdopen(control, "r");
+
+  fgets(str, size, fp);
+  printf("%s", str);
   
+  return 0;
+
+}
+
+int loginFTP(const char* user, const char* password, sockets* ftp){
+  char userTest[STRING_SIZE];
+  char passTest[STRING_SIZE];
+
+  sprintf(userTest, "USER %s\r\n", user);
+  sprintf(passTest, "PASS %s\r\n", password);
+
+  if(write(ftp->control, userTest, strlen(userTest))==-1){
+    printf("Login failed!\n");
+    return -1;
+  }
+
+  if(readFtpReply(ftp->control, userTest, STRING_SIZE)){
+    printf("Login failed!\n");
+    return -1;
+  }
+
+  if(write(ftp->control, passTest, strlen(passTest))==-1){
+    printf("Login failed!\n");
+    return -1;
+  }
+
+  if(readFtpReply(ftp->control, passTest, STRING_SIZE)){
+    printf("Login failed!\n");
+    return -1;
+  }
+
+  return 0;
+}
+
+
+int cdFTP(const char* path, sockets* ftp){
+
+  char currPath[STRING_SIZE];
+
+  sprintf(currPath, "CWD %s\r\n", path);
+
+  if(write(ftp->control, currPath, strlen(currPath))==-1){
+    printf("Sending to FTP failed - changeDirFTP().\n");
+    return 1;
+  }
+
+
+  if(readFtpReply(ftp->control, currPath, STRING_SIZE)){
+    printf("Read from FTP failed - changeDirFTP().\n");
+    return 1;
+  }
+
+  return 0;
+}
+
+int passiveMode(sockets* ftp){
+
+  char passive[STRING_SIZE];
+  char passiveIp[STRING_SIZE];
+  sprintf(passive, "PASV\n");
+
+  if(write(ftp->control, passive, strlen(passive)))
+  {
+    printf("Passive mode failed.\n");
+    return 1;
+  }
+
+  if(readFtpReply(ftp->control, passive, STRING_SIZE))
+  {
+    printf("Passive mode failed.\n");
+    return 1;
+  }
+  
+  int ip1,ip2,ip3,ip4;
+  int port1, port2;
+
+  if((sscanf(passive,"227 Entering Passive Mode (%d,%d,%d,%d,%d,%d)", &ip1,&ip2,&ip3,&ip4,&port1,&port2)) < 0)
+  {
+    printf("Incorrect response.");
+    return 1;
+  }
+
+  sprintf(passiveIp,"%d.%d.%d.%d",ip1,ip2,ip3,ip4);
+
+  int port = port1*256 + port2;
+
+  if((ftp->data = connectFTP(passiveIp, port)) < 0)
+  {
+    printf("Passive mode cannot be entered.");
+    return 1;
+  }
+  return 0;
+
+}
+
+int copyFileFTP(const char* filename, sockets* ftp)
+{
+  char retr[STRING_SIZE];
+
+  sprintf(retr, "RETR %s\n", filename);
+
+  if(write(ftp->control, retr, strlen(retr))==-1){
+    printf("Sending failed.\n");
+    return 1;
+  }
+
+
+  if(readFtpReply(ftp->control, retr, STRING_SIZE)){
+    printf("Sending failed.\n");
+    return 1;
+  }
+  return 0;
+}
+
+
+int downloadFileFTP(const char* filename, sockets* ftp){
+
+  FILE* fp;
+  int bytes;
+
+  if (!(fp = fopen(filename, "w"))) 
+  {
+    printf("Download file.\n");
+    return 1;
+  }
+
+  char buf[STRING_SIZE];
+
+  while((bytes = read(ftp->data, buf, sizeof(buf))))
+  {
+    if (bytes < 0) {
+      printf("Download failed.\n");
+      return 1;
+    }
+
+    if((bytes = fwrite(buf,bytes, 1 , fp)) < 0 )
+    {
+      printf("Download failed.\n");
+      return 1;
+    }
+  }
+
+  fclose(fp);
+  close(ftp->data);
+  return 0;
+
+}
+
+int disconnectFromFTP(sockets* ftp){
+
+  char disc[STRING_SIZE];
+  sprintf(disc, "QUIT\n");
+  
+  if(write(ftp->control, disc, strlen(disc)))
+  {
+    printf("Sending failed.\n");
+    return 1;
+  }
+
+  if(readFtpReply(ftp->control, disc, STRING_SIZE))
+  {
+    printf("Sending failed.\n");
+    return 1;
+  }
+
+  return 0;
+
 }
 
 int main(int argc, char** argv){
-
-  //printf("%d\n", argc);
-
-	// int	sockfd;
-	// struct	sockaddr_in server_addr;
-	// char	buf[] = "Mensagem de teste na travessia da pilha TCP/IP\n";
-	// int	bytes;
-
-	/*server address handling*/
-	// bzero((char*)&server_addr,sizeof(server_addr));
-	// server_addr.sin_family = AF_INET;
-	// server_addr.sin_addr.s_addr = inet_addr(SERVER_ADDR);	/*32 bit Internet address network byte ordered*/
-	// server_addr.sin_port = htons(SERVER_PORT);		/*server TCP port must be network byte ordered */
-
-	/*open an TCP socket*/
-	// if ((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0) {
-  //   		perror("socket()");
-  //       	exit(0);
-  //   	}
-	/*connect to the server*/
-  //   	if(connect(sockfd,
-	//            (struct sockaddr *)&server_addr,
-	// 	   sizeof(server_addr)) < 0){
-  //       	perror("connect()");
-	// 	exit(0);
-	// }
-    	/*send a string to the server*/
-	// bytes = write(sockfd, buf, strlen(buf));
-	// printf("Bytes escritos %d\n", bytes);
-
-  //getip.c
 
   if (argc != 2) {
       //fprintf(stderr,"usage: getip address\n");
@@ -131,11 +322,30 @@ int main(int argc, char** argv){
   if(parseURL(argv[1], &info)==-1)
     exit(0);
 
-    printf("%s\n", info.user);
-    printf("%s\n", info.password);
-    printf("%s\n", info.host);
-    printf("%s\n", info.path);
+  printf("%s\n", info.user);
+  printf("%s\n", info.password);
+  printf("%s\n", info.host);
+  printf("%s\n", info.path);
 
-	//close(sockfd);
+  getIpByHost(&info);
+
+  int port = 21;
+
+  sockets socket;
+
+  connectFTP(info.ip, port);
+
+  loginFTP(info.user, info.password, &socket);
+
+  cdFTP(info.path, &socket);
+
+  passiveMode(&socket);
+
+  copyFileFTP(info.file, &socket);
+
+  downloadFileFTP(info.file, &socket);
+
+  disconnectFromFTP(&socket);
+
 	exit(0);
 }
--- Project2/ftp.h
+++ Project2/ftp.h
@@ -20,8 +20,25 @@ typedef struct{
   char *host;
   char *path;
   char *file;
+  char *ip;
 }url_info;
+
+typedef struct{
+	int data;
+	int control;
+	
+}sockets;
 
 int parseURL(char *path, url_info *info);
 
-int connect(const char *ip, int port);
+int connectFTP(const char *ip, int port);
+
+int loginFTP(const char* user, const char* password, sockets* ftp);
+
+int getIpByHost(url_info* url);
+
+int readFtpReply(int control, char* str, size_t size);
+
+int passiveMode(sockets* ftp);
+
+int copyFileFTP(const char* filename, sockets* ftp);
